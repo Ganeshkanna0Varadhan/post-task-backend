c9ad13a2c4b26035f3141667786607c5d95f0b06
Ganeshkanna0Varadhan <ganeshkanna718@gmail.com>
2025-04-27 14:32:43 +0530

    connect URI updated

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect(process.env.DB_URI, {
-        //     connectTimeoutMS: 10000
-        // })
-        await mongoose.connect(process.env.DEV_DB_URI, {
+        await mongoose.connect(process.env.DB_URI, {
             connectTimeoutMS: 10000
-        });
-        
+        })
+        // await mongoose.connect(process.env.DEV_DB_URI, {
+        //     connectTimeoutMS: 10000
+        // });
         console.log('Database connect successfully');
     }
     catch(err) {
